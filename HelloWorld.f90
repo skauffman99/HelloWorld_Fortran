e2fe2463536b3a9f9c0f822a094c681d821e959a
S. Kauffman <skauffman@urban.org>
2019-05-23 08:51:04 -0400

    mvoing calculation to subroutine

--- HelloWorld.f90
+++ HelloWorld.f90
@@ -20,9 +20,11 @@ program HelloAgainWorldF90
   read(99,*) x 			!now read in the array
 
   !do something to the array, like loop over it and multiply by 2
-  do i=0,n
-   x(i) = x(i) * 2
-  end do
+  subroutine calculate
+    do i=0,n
+    x(i) = x(i) * 2
+    end do
+  end subroutine
 
   write(*,*) "Done my calculations."
   open(unit=89, file='IAmHere.txt', status='replace', action='write')
